bf1462e97a94a988b352e743f13b2ba9cd8aaa24
Matt Dolian <mdolian@engineyard.com>
2008-12-06 18:10:13 -0500

    Added venue filter

--- app/controllers/shows.rb
+++ app/controllers/shows.rb
@@ -7,7 +7,15 @@ class Shows < Application
   end
   
   def search
-    @shows = Show.all(:conditions => {:date_played.gte => params['year'], :date_played.lt => (params['year'].to_i+1).to_s})
+    @venues - Venue.all
+  end
+  
+  def search_results
+    options = {:date_played.gte => params['year'], :date_played.lt => (params['year'].to_i+1).to_s}
+    if !params["venue_name"].empty?
+      options = {:date_played.gte => params['year'], :date_played.lt => (params['year'].to_i+1).to_s, 'venue.venue_name' => params["venue_name"]}
+    end
+    @shows = Show.all(:conditions => options)
     render
   end
   
