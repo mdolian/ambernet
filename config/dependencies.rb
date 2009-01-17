0719b6064f6381a5b2d4bd28f57e67c3c8792791
Matt Dolian <dolian@MDmbPro.local>
2009-01-17 16:22:51 -0500

    Updated to Merb 1.0.8

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,33 +1,37 @@
-merb_version = "=1.0.7.1"
-dm_version   = "=0.9.9"
+merb_gems_version = "1.0.8"
+dm_gems_version   = "0.9.9"
+do_gems_version   = "0.9.10.1"
 
-dependency "dm-core",             dm_version
-dependency "dm-validations",      dm_version
-dependency "dm-timestamps",       dm_version
-dependency "dm-migrations",       dm_version
-dependency "dm-serializer",       dm_version
-dependency "dm-types",            dm_version
+dependency "merb-action-args", merb_gems_version
+dependency "merb-assets", merb_gems_version  
+dependency "merb-cache", merb_gems_version   
+dependency "merb-helpers", merb_gems_version 
+dependency "merb-mailer", merb_gems_version  
+dependency "merb-haml", merb_gems_version 
+dependency "merb-slices", merb_gems_version  
+dependency "merb-auth-core", merb_gems_version
+dependency "merb-auth-more", merb_gems_version
+dependency "merb-auth-slice-password", merb_gems_version
+dependency "merb-param-protection", merb_gems_version
+dependency "merb-exceptions", merb_gems_version
 
-dependency "merb-core",               merb_version
-dependency "merb-gen",                merb_version
-dependency "merb-helpers",            merb_version
-dependency "merb-haml",               merb_version
-dependency "merb-auth-core",          merb_version
-dependency "merb-auth-more",          merb_version
-dependency "merb-auth-slice-password",merb_version
-dependency "merb-assets",             merb_version
-dependency "merb-slices",             merb_version
-dependency "merb-action-args",        merb_version
-dependency "merb_datamapper",         merb_version
-dependency "merb-param-protection",   merb_version
+dependency "data_objects", do_gems_version
+dependency "do_mysql", do_gems_version
+dependency "dm-core", dm_gems_version         
+dependency "dm-aggregates", dm_gems_version   
+dependency "dm-migrations", dm_gems_version   
+dependency "dm-timestamps", dm_gems_version   
+dependency "dm-types", dm_gems_version        
+dependency "dm-validations", dm_gems_version  
+dependency "dm-serializer", dm_gems_version   
+
+dependency "merb_datamapper", merb_gems_version
 
 dependency 'nokogiri'
-dependency 'webrat', '=0.3.2'
+dependency 'webrat'
 dependency "thin"
 dependency 'rake'
 dependency 'rcov'
 dependency 'hoe'
 dependency 'haml'
 dependency "extlib"
-dependency "data_objects"
-dependency "do_mysql"
