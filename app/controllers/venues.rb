6fbcaa29fc5852a06a11de1e87a1eb5510333f0a
Matt Dolian <dolian@MDmbPro.local>
2009-01-27 00:19:33 -0500

    fixed autocomplete

--- app/controllers/venues.rb
+++ app/controllers/venues.rb
@@ -9,7 +9,7 @@ class Venues < Application
     venues = Venue.all(:conditions => {:venue_name.like => params["q"] << "%"})
     list = ""
     venues.each do |venue|
-      list << venue.venue_name << " "
+      list << venue.venue_name << "\n"
     end
     render list, :layout => false
   end
@@ -18,7 +18,7 @@ class Venues < Application
     venues = Venue.all(:conditions => {:venue_city.like => params["input_content"] << "%"})
     list = ""
     venues.each do |venue|
-      list << venue.venue_city
+      list << venue.venue_city << "\n"
     end
     render list, :logout => false
   end
