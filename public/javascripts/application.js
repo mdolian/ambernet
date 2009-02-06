14dae79b519edff0934da0558f3720bad679fb72
Matt Dolian <dolian@MDmbPro.local>
2009-02-05 20:18:55 -0500

    more troubleshooting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,10 +5,8 @@ $(function() {
     $("#carousel_block").cycle({
 	    fx:     'scrollHorz', 
 	    speed:  'fast',
-	    prev:   '#prev',
-	    next:   '#next',
 	    after:   onAfter,
-	    pager:  '#carousel-control',
+	    #pager:  '#carousel-control',
 	    timeout: 0,
       pagerAnchorBuilder: function(idx, slide) {
           return '#carousel-control a:eq(' + (idx) + ')';
