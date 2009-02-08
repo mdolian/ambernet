38cfffe896aec2c9df233f982191e366d49552ca
Matt Dolian <dolian@MDmbPro.local>
2009-02-07 21:08:08 -0500

    changed staging log mode to debug

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ Merb.logger.info("Loaded STAGING Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes]    = false
-  c[:log_level]         = :error
+  c[:log_level]         = :debug
 
   c[:log_file]  = Merb.root / "log" / "staging.log"
   # or redirect logger using IO handle
