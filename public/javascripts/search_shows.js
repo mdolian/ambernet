a7e46c334a16e98d44d6058ea01162f22d963143
Matt Dolian <dolian@MDmbPro.local>
2009-05-06 15:08:56 -0400

    updated shows search to use loading image

--- public/javascripts/search_shows.js
+++ public/javascripts/search_shows.js
@@ -1,14 +1,16 @@
 $(function() {
   jQuery(document).ready(function($) {
 		$('#search_form_shows').ajaxForm({
+		   beforeSubmit: displayLoadingImage,			
 			 success: clearSearchButtons,
 		 	 target: '#search_accordion_shows'
 		});
 		$('#paginate_form_shows').ajaxForm({
+		   beforeSubmit: displayLoadingImage,			
 		 	 target: '#search_accordion_shows'
 		});
 	  $('div.paginated a').livequery('click', function() {
-		  $('#search_accordion_shows').html('<p><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /></p>');
+      displayLoadingImage();   
 		  $.ajax({
 			  type: "POST",
 			  url:  this.href,
@@ -20,8 +22,8 @@ $(function() {
 	  });
 	  $('#recording_details_shows').livequery('click', function() {
 			$("div[id*='dialog_rec']").dialog('close');
-			$("div[id*='dialog_setlist']").dialog('close');		
-		  $('#search_accordion_shows').html('<p><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /></p>');		
+			$("div[id*='dialog_setlist']").dialog('close');	
+			displayLoadingImage();	
 		  $.ajax({
 			  type: "POST",
 			  url:  this.href,
@@ -34,5 +36,8 @@ $(function() {
 	});		
 	function clearSearchButtons(responseText, statusText)  { 
 	    $('#search_buttons_shows').empty();
+	}	
+	function displayLoadingImage() {
+		$('#search_accordion_recordings').html('<p><br><br><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /><br><br><br><br><br></p>');
 	}	
 });		
