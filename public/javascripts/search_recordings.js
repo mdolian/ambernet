980766b661511e74819b6b41cb8697b40edfc3ce
Matt Dolian <dolian@MDmbPro.local>
2009-05-06 15:02:21 -0400

    added loading image for ajax

--- public/javascripts/search_recordings.js
+++ public/javascripts/search_recordings.js
@@ -1,22 +1,41 @@
 $(function() {
   jQuery(document).ready(function($) {
 		$('#search_form_recordings').ajaxForm({
+		   beforeSubmit: displayLoadingImage,
 			 success: clearSearchButtons,
 		 	 target: '#search_accordion_recordings'
 		});
 		$('#paginate_form_recordings').ajaxForm({
+		   beforeSubmit: displayLoadingImage,			
 		 	 target: '#search_accordion_recordings'
 		});	
 	  $('div.paginated a').livequery('click', function() {
-	    $('#search_accordion_recordings').load(this.href);
+			displayLoadingImage();
+		  $.ajax({
+			  type: "POST",
+			  url:  this.href,
+			  success: function(msg) {
+				  $('#search_accordion_recordings').html(msg);
+			  }	
+		  })
 	    return false;
 	  });
 	  $('#recording_details').livequery('click', function() {
-	    $('#search_accordion_recordings').load(this.href);
+			displayLoadingImage();
+		  $.ajax({
+			  type: "POST",
+			  url:  this.href,
+			  success: function(msg) {
+				  $('#search_accordion_recordings').html(msg);
+			  }	
+		  })		
 	    return false;
 	  });				
 	});	
-	function clearSearchButtons(responseText, statusText)  { 
-	    $('#search_buttons_recordings').empty();
-	}	
+	function clearSearchButtons()  { 
+	  $('#search_buttons_recordings').empty();
+	}
+	function displayLoadingImage() {
+		$('#search_accordion_recordings').html('<p><br><br><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /><br><br><br><br><br></p>');
+	}
 });
--- public/javascripts/search_shows.js
+++ public/javascripts/search_shows.js
@@ -8,13 +8,27 @@ $(function() {
 		 	 target: '#search_accordion_shows'
 		});
 	  $('div.paginated a').livequery('click', function() {
-	    $('#search_accordion_shows').load(this.href);
+		  $('#search_accordion_shows').html('<p><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /></p>');
+		  $.ajax({
+			  type: "POST",
+			  url:  this.href,
+			  success: function(msg) {
+				  $('#search_accordion_shows').html(msg);
+			  }	
+		  })
 	    return false;
 	  });
 	  $('#recording_details_shows').livequery('click', function() {
-	    $('#search_accordion_shows').load(this.href);
 			$("div[id*='dialog_rec']").dialog('close');
-			$("div[id*='dialog_setlist']").dialog('close');
+			$("div[id*='dialog_setlist']").dialog('close');		
+		  $('#search_accordion_shows').html('<p><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /></p>');		
+		  $.ajax({
+			  type: "POST",
+			  url:  this.href,
+			  success: function(msg) {
+				  $('#search_accordion_shows').html(msg);
+			  }	
+		  })	   
 	    return false;
 	  });						
 	});		
