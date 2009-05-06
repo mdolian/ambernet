afb5821036a254788abc035dbf49fa12387f6fa7
Matt Dolian <mdolian@engineyard.com>
2009-05-06 16:33:51 -0400

    added loading image for tabs

--- app/controllers/amberland.rb
+++ app/controllers/amberland.rb
@@ -4,4 +4,7 @@ class Amberland < Application
     render
   end
   
+  def info
+    render :layout => false
+  end      
 end
--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -1,5 +1,12 @@
 $(function() {
   jQuery(document).ready(function($) {	
-	  $("#index_tabs").tabs();
+	  $("#index_tabs").tabs({
+			ajaxOptions: { 
+				async: false,
+				beforeSend: function() {
+					$("div[id*='main']").html('<p align="center"><br><br><br><br><br><img src="/images/loading_bar.gif" width="220" height="19" /><br><br><br><br><br></p>');
+				},
+			}			
+		});
   });		
 });
