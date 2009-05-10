7adfef923fb8a6c1956d9f7bb03a154d5a57fe2d
Matt Dolian <dolian@MDmbPro.local>
2009-05-09 21:41:55 -0400

    debugging zip file

--- app/controllers/recordings.rb
+++ app/controllers/recordings.rb
@@ -175,7 +175,8 @@ class Recordings < Application
         Merb.logger.debug "File added to zip: #{file.path}"    
       end
     end
-    send_file "/zips/#{@recording.id}-#{request.remote_ip}.zip", :type => 'application/zip', :disposition => 'attachment', :filename => "#{@recording.label}.#{type}.zip"
+    puts "Temp Zip Path: #{t.path}"
+    send_file t.path, :type => 'application/zip', :disposition => 'attachment', :filename => "#{@recording.label}.#{type}.zip"
     t.close    
   end
 
