167c631fe73a3dce6f5eaf38691c4c3054cd3797
Matt Dolian <dolian@MDmbPro.local>
2009-05-10 20:46:19 -0400

    another zip fix

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -96,7 +96,7 @@ class Recording
    
   def files(type)
     tracks do |track|
-      yield File.open("/ambernet/#{label}/#{track}.#{download_extension(type)}") 
+      yield File.open("/media/PG_Archive/ambernet/#{label}/#{track}.#{download_extension(type)}") 
     end
   end
 end
