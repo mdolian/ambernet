ef4839a0116f35152c7195b7959ce02e12276da6
Matt Dolian <dolian@MDmbPro.local>
2009-05-17 21:28:17 -0400

    trying zip link js in master.js, clicking the links generates multiple requests

--- public/javascripts/show_recording.js
+++ public/javascripts/show_recording.js
@@ -10,37 +10,6 @@ $(function() {
 		  })		
 		  return false;
 		});		
-		$('#zip_link_mp3').livequery('click', function() {
-		  $.ajax({
-				url: '/recordings/zip/' + $('#recording_id').html()  + '/mp3',
-				success: function(msg) {
-					$('#zip_mp3').html(msg);
-					var m = setInterval(function() {
-				     $('#zip_mp3').load('/recordings/zip_link/' + $('#recording_id').html() + '/mp3');
-				     if($('#status_mp3').html() == null || $('#status_mp3').html() == "") {
-					   } else {
-							 clearInterval(m);			
-				     }
-				  }, 30000);				
-				}			
-			})
-		  return false;
-		});	
-		$('#zip_link_lossless').livequery('click', function() {
-		  $.ajax({
-				url: '/recordings/zip/' + $('#recording_id').html()  + '/lossless',
-				success: function(msg) {
-					$('#zip_lossless').html(msg);
-					var l = setInterval(function() {
-				     $('#zip_lossless').load('/recordings/zip_link/' + $('#recording_id').html() + '/lossless');
-				     if($('#status_lossless').html() == null || $('#status_lossless').html() == "") {
-							 clearInterval(l);			
-				     }		  
-				  }, 60000);				
-				}
-			})
-		  return false;
-		});
 	  $('#back').livequery('click', function() {
 			displayLoadingImage();
 		  $.ajax({
