7d6bb64fb00ee69cf4eb6f00a796527bacecbadd
Matt Dolian <dolian@MDmbPro.local>
2009-07-24 18:34:46 -0400

    added a plugin to load a sql.gz file, rake tasks seem to be broke with dm

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
+  config.frameworks -= [ :active_record ]
+
   dm_gems_version = "0.9.11"
   do_gems_version = "0.9.12"
 
@@ -19,18 +21,18 @@ Rails::Initializer.run do |config|
   config.gem "dm-timestamps", :version => dm_gems_version
   config.gem "dm-types", :version => dm_gems_version
   config.gem "dm-validations", :version => dm_gems_version
-  config.gem "dm-serializer", :version => dm_gems_version  
+  config.gem "dm-serializer", :version => dm_gems_version 
+  config.gem "rails_datamapper", :version => dm_gems_version   
   config.gem "haml"
   config.gem "dm-is-paginated"
   config.gem "merb-pagination"
   config.gem "hpricot"
   config.gem "rubyzip", :lib => "zip/zip"
+  config.gem "rails_datamapper"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  config.frameworks -= [ :active_record ]
 
   config.time_zone = 'UTC'
 
