ca56ec8435e06c462d383fbd7e504c5990a7e2ef
Matt Dolian <dolian@MDmbPro.local>
2010-02-12 19:05:11 -0500

    updated queries in recordings and shows controller to be thinking sphinx or active record, could not test

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -19,7 +19,7 @@ class RecordingsController < ApplicationController
       conditions.merge!({Recording.show.date_played.gte => params["year"], 
                          Recording.show.date_played.lt => (params["year"].to_i+1).to_s})   if params["year"] != "All"
       @recordings = Recording.all(:conditions => conditions)    
-        render :admin
+      render :admin
     else
       render :year_list
     end
@@ -27,7 +27,7 @@ class RecordingsController < ApplicationController
 
   # deletes a recording  
   def delete
-    Recording.get(params["id"]).destroy
+    Recording.find(params["id"]).destroy
     redirect_to "/recordings/admin"
   end
   
@@ -39,7 +39,7 @@ class RecordingsController < ApplicationController
         tracking_info << params["tracksDisc" << i.to_s] << ","
       end
       tracking_info.chop! << "]"    
-      @recording = Recording.get(params["id"])
+      @recording = Recording.find(params["id"])
       @recording.update_attributes( :label => params["label"],
                                     :source => params["source"],
                                     :lineage => params["lineage"],
@@ -52,7 +52,7 @@ class RecordingsController < ApplicationController
                                     :tracking_info => tracking_info)                                                                                             
       redirect_to "/recordings/admin"
     else  
-       @recording = Recording.get(params["id"])
+       @recording = Recording.find(params["id"])
        render 
     end
   end
@@ -105,16 +105,16 @@ class RecordingsController < ApplicationController
     format, id = params["format"], params["id"]
     if params["id"].length > 4 
       stream = ""
-      if Recording.count(Recording.show.date_played => id) > 0
-        Recording.all(Recording.show.date_played => id).each do |recording|
+      if Recording.count(:join => :shows, conditions => {:date_played => id}) > 0
+        Recording.all(:date_played => id).each do |recording|
           stream << format == "pls" ? recording.to_pls : recording.to_m3u << "\n\n"
         end
       else
         render "Sorry, no show exists for that date", :layout => false
       end
     else
-      stream = params["format"] == "pls" ? Recording.get(params["id"]).to_pls : Recording.get(params["id"]).to_m3u
-      label = Recording.get(params["id"]).label
+      stream = params["format"] == "pls" ? Recording.find(params["id"]).to_pls : Recording.find(params["id"]).to_m3u
+      label = Recording.find(params["id"]).label
     end
     content_type = "application/m3u" if params["format"] == "m3u"
     content_type = "application/pls" if params["format"] == "pls"
@@ -124,7 +124,7 @@ class RecordingsController < ApplicationController
 
   # recording details action
   def show
-    @recording = Recording.get(params["id"])
+    @recording = Recording.find(params["id"])
     render
   end
  
@@ -137,51 +137,48 @@ class RecordingsController < ApplicationController
 
     if params["submit"] != nil
       # Need to get drop downs working in views
-      #conditions.merge!({:type => params["type"]})                                                          if params["type"] != 'all'
-      #conditions.merge!({:shnid => params["shnid"]})                                                        if params["shnid"] != ''      
-      #conditions.merge!({:label.like => "%" << params["label"] << "%"})                                     if params["label"] != ''
-      #conditions.merge!({:source.like => "%" << params["source"] << "%"})                                   if params["source"] != ''
-      #conditions.merge!({:lineage.like => "%" << params["lineage"] << "%"})                                 if params["lineage"] != ''
-      #conditions.merge!({:taper.like => "%" << params["taper"] << "%"})                                     if params["taper"] != ''
-      #conditions.merge!({"show.date_played >= #{params['year']} AND show.date_played < (#{params['year'].to_i+1).to_s}"})        if params["year"] != 'All'
-      #conditions.merge!({Recording.show.venue.venue_name.like => "%" << params["venue_name"] << "%"})       if params["venue_name"] != ''
-      #conditions.merge!({Recording.show.venue.venue_city.like => "%" << params["venue_city"] << "%"})       if params["venue_city"] != ''
-      #conditions.merge!({Recording.show.venue.venue_state.like => "%" << params["venue_state"] << "%"})     if params["venue_state"] != ''
-      #conditions.merge!({Recording.show.setlists.song.song_name.like => "%" << params["song_name"] << "%"}) if params["song_name"] != ''
+      conditions.merge!({:type => params["type"]})                                  if params["type"] != 'all'
+      conditions.merge!({:shnid => params["shnid"]})                                if params["shnid"] != ''      
+      conditions.merge!({:label => params["label"], :star => true})                 if params["label"] != ''
+      conditions.merge!({:source => params["source"], :star => true})               if params["source"] != ''
+      conditions.merge!({:lineage => params["lineage"], :star => true})             if params["lineage"] != ''
+      conditions.merge!({:taper => params["taper"], :star => true})                 if params["taper"] != ''
+      conditions.merge!({:venue_name => params["venue_name"], :star => true})       if params["venue_name"] != ''
+      conditions.merge!({:venue_city => params["venue_city"], :star => true})       if params["venue_city"] != ''
+      conditions.merge!({:venue_state => params["venue_state"], :star => true})     if params["venue_state"] != ''
+      conditions.merge!({:song_name => params["song_name"], :star => true})         if params["song_name"] != ''
 
-      #unless (params["end_date"] == '' && params["start_date"] == '')
-      #  end_date = params["end_date"] == '' ? Date.today : Date.parse(params["end_date"])       
-      #  start_date = params["start_date"] == '' ? Date.today : Date.parse(params["start_date"])
-      #  error_message = "Start date later than end date" if (end_date < start_date)   
-         
-      #  conditions.merge!({Recording.show.date_played.gte => params["start_date"], 
-       #                                Recording.show.date_played.lte => params["end_date"]})      
-      #end 
+      unless (params["end_date"] == '' && params["start_date"] == '')
+        end_date = params["end_date"] == '' ? Date.today : Date.parse(params["end_date"])       
+        start_date = params["start_date"] == '' ? Date.today : Date.parse(params["start_date"])
+        error_message = "Start date later than end date" if (end_date < start_date)   
+        conditions.merge!({:date_played  => start_date..end_date})    
+      end 
     else
       conditions = session[:conditions]
     end
     
     error_message = "Please select at least one search filter" if conditions.empty?  
+  
+    if error_message == ''
+      @recordings = Recording.paginate(:conditions => conditions, :page => @current_page, 
+                                       :order => :date_played, :sort_mode => :asc)                
+      session[:conditions] = conditions 
+    end
     
-    # Need to implement mislav's will_paginate
-    #@page_count, @recordings = Recording.paginated(
-    #  :page => @current_page,
-    #  :per_page => 50,
+    if error_message != ''
+      flash.now[:error] = error_message
+      flash.now[:notice] = notice_message
+    end
     
-    @recordings = Recording.paginate(
-      :page => @current_page, 
-      :conditions => conditions)                if error_message == ''
-    #notice_message = "No recordings were found" if @recordings.count == 0  
-    session[:conditions] = conditions           if error_message == '' 
-    flash.now[:error] = error_message           if error_message != ''
-    flash.now[:notice] = notice_message         if notice_message != ''
     render
+  
   end
   
   # TO-DO - convert to Rails
   def zip
     only_provides :zip
-    @recording = Recording.get(params["id"])
+    @recording = Recording.find(params["id"])
     if !File.exist?("public/ambernet/zips/#{@recording.label}.#{params['type']}.zip")
       t = File.open("public/ambernet/zips/#{@recording.label}.#{params['type']}.zip", "w")
       Zip::ZipOutputStream.open(t.path) do |zos|
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -14,7 +14,7 @@ class ShowsController < ApplicationController
   
   def list
     list = []
-    shows = Show.all(:date_played.eql => Date.strptime(params["date_played"]))
+    shows = Show.all(["date_played = " << Date.strptime(params["date_played"])])
     shows.each do |show|
       list << {"label" => show.label, "id" => show.id}
     end
@@ -23,16 +23,16 @@ class ShowsController < ApplicationController
  
   def setlist
     setlist_json = []
-    total_sets = Show.get(params["id"]).setlists[0].total_sets
-    Show.get(params["id"]).setlists.each do |setlist|
-      song = Song.get(setlist.song_id)
+    total_sets = Show.find(params["id"]).setlists[0].total_sets
+    Show.find(params["id"]).setlists.each do |setlist|
+      song = Song.find(setlist.song_id)
       setlist_json << {"set_id" => setlist.set_id, "song_order" => setlist.song_order, "song_name" => song.song_name, "segue" => setlist.song_suffix, "total_sets" => total_sets}
     end
     render :json => setlist_json.to_json, :layout => false
   end
  
   def recordings
-    @recordings = Recording.all(:conditions => {:show_id => params["id"]})
+    @recordings = Recording.all(:show_id => params["id"])
     render :layout => false
   end
     
@@ -42,34 +42,38 @@ class ShowsController < ApplicationController
     error_message, notice_message = "",""
     
     if params["submit"] != nil
-      #conditions.merge!({Show.setlists.song.song_name.like => "%" << params["song_name"] << "%"}) if params["song_name"] != ''
-      #conditions.merge!({:date_played.gte => params["year"],
-      #                   :date_played.lt => (params["year"].to_i+1).to_s}) if params["year"] != 'All'
-      #conditions.merge!({Show.venue.venue_city.like => "%" << params["venue_city"] << "%"}) if params["venue_city"] != ''
-      #conditions.merge!({Show.venue.venue_state => "%" << params["venue_state"] << "%"}) if params["venue_state"] != ''
-      #conditions.merge!({Show.venue.venue_name.like => "%" << params["venue_name"] << "%"}) if params["venue_name"] != ''
+      conditions.merge!({:song_name => params["song_name"], :star => true})                           if params["song_name"] != ''
+      conditions.merge!({:date_played => params["year"].to_date..(params["year"].to_i+1).to_date})    if params["year"] != 'All'
+      conditions.merge!({:venue_city => params["venue_city"], :star => true})                         if params["venue_city"] != ''
+      conditions.merge!({:venue_state => params["venue_state"], :star => true})                       if params["venue_state"] != ''
+      conditions.merge!({:venue_name => params["venue_name"], :star => true})                         if params["venue_name"] != ''
  
-      #unless (params["end_date"] == '' && params["start_date"] == '')
-      #  end_date = params["end_date"] == '' ? Date.today : Date.parse(params["end_date"])
-      #  start_date = params["start_date"] == '' ? Date.today : Date.parse(params["start_date"])
-      #  error_message = "Start date later than end date" if (end_date < start_date)
-      #  conditions.merge!({:date_played.gte => start_date, :date_played.lte => end_date})
-      #end
+      unless (params["end_date"] == '' && params["start_date"] == '')
+        end_date = params["end_date"] == '' ? Date.today : Date.parse(params["end_date"])
+        start_date = params["start_date"] == '' ? Date.today : Date.parse(params["start_date"])
+        error_message = "Start date later than end date" if (end_date < start_date)
+        conditions.merge!({:date_played  => start_date..end_date})
+      end
     else
       conditions = session[:conditions]
     end
     
     error_message = "Please select at least one search filter" if conditions.empty?
+    :page => params[:page], :per_page => 42
     
-    @shows = Show.paginate(
-      :page => @current_page,
-      :conditions => conditions,
-      :order => [:date_played.asc]) if error_message == ''
-    session[:conditions] = conditions if error_message == ''
-    session[:searchBranch] = "shows" if error_message == ''
-    session[:current_page] = @current_page if error_message == ''
-    flash[:error] = error_message if error_message != ''
-    flash[:notice] = notice_message if notice_message != ''
+    if error_message == ''
+    
+      @shows = Show.search(:conditions => conditions, :page => @current_page, 
+                           :order => :date_played, :sort_mode => :asc)  
+      session[:conditions] = conditions                        
+      session[:current_page] = @current_page
+    end
+    
+    if error_message != ''
+      flash[:error] = error_message
+      flash[:notice] = notice_message
+    end
+    
     render
  
   end
