17fad001fbb010eb02b930863bf9280a520935be
Matt Dolian <dolian@MDmbPro.local>
2010-03-22 20:38:35 -0400

    cleaned up jquery shit

--- public/javascripts/accordion.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  jQuery(document).ready(function($) {
-		$("div[id*='accordion']").accordion({
-			autoHeight: false, 
-			collapsible: true
-		});
-  });				
-});		
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
 $(function() {
   jQuery(document).ready(function($) {
   	$("#nav ul li a").corner();
+  });
+  jQuery(document).ready(function($) {
+		$("div[id*='accordion']").accordion({
+			autoHeight: false, 
+			collapsible: true
+		});
   });		
 });
