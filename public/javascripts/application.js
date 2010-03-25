54250489630a2d59b790ad11dd9b2150638445f9
Matt Dolian <dolian@MDmbPro.local>
2010-03-24 22:17:14 -0400

    changed nav menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,8 @@ $(function() {
   jQuery(document).ready(function($) {
   	$("#nav ul li a").corner();
 
+		$("#lavaLamp").lavaLamp();
+
 		$("div[id*='accordion']").accordion({
 			autoHeight: false, 
 			collapsible: true
@@ -29,6 +31,59 @@ $(function() {
 		$("a", ".wizard").click(function() { return false; });
 
 		$('input:checkbox, input:radio').checkbox();
+	
+		$('#start_date').datepicker({
+			dateFormat: 'yy-mm-dd',
+			changeMonth: true,
+			changeYear: true	
+		});
+			
+		$('#end_date').datepicker({
+			dateFormat: 'yy-mm-dd',
+			changeMonth: true,
+			changeYear: true
+		});		
+
+	  $("div[id*='dialog_setlist']").dialog({ autoOpen: false, width: 400, modal: true });
+	  $("div[id*='dialog_rec']").dialog({ autoOpen: false, width: 400, modal: true });		
+		$.fn.setlist = function(show_id){
+			that = this
+			var set = new Array();
+			$.getJSON("/shows/setlist/" + show_id, function(json){
+				set[1] = "<b>Set 1:</b> "
+				set[2] = "<b>Set 2:</b> "
+				set[3] = "<b>Set 3:</b> "
+				set[4] = "<b>Set 4:</b> "
+				set[5] = "<b>Set 5:</b> "												
+				set[9] = "<b>Encore:</b> "
+				total_sets = 0		
+				$.each(json, function(i, item) {
+					total_sets = item.total_sets
+					set[item.set_id] += item.song_name + item.segue;
+				});
+				html = "<p>"
+				for(i=1; i<=total_sets; i++) {
+					html += set[i] + "<br/>"
+				}
+				if(set[9] != "<b>Encore:</b> ") {
+				  html += set[9]
+				}
+				html += "</p>";
+			  $(that).html(html);
+			  $(that).dialog("open");
+			});								
+		}
+		
+		$.fn.recordings = function(show_id){
+			that = this; 
+		  $.ajax({
+			  url:  "/shows/recordings/" + show_id,
+			  success: function(msg) {
+				  $(that).html(msg);
+				  $(that).dialog("open");
+			  }	
+		  })						
+		}
 		
   });	
 });
--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -1,5 +1,7 @@
 $(function() {
   jQuery(document).ready(function($) {
+		$('li').addClass('current');
+
 		$("#browseForm").formwizard({ 
 			validationEnabled : true,
 			focusFirstInput : true
