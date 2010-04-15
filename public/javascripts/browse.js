d944689a257c3a2d7f4efa8db2c64239a5c5ffc0
Matt Dolian <dolian@MDmbPro.local>
2010-04-14 20:01:34 -0400

    some form wizard changes

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -3,7 +3,21 @@ $(function() {
 		$("#browseForm").formwizard();
 		
 		$('.navigation_button').bind('click', function() {
-			alert($('input[name=browse]:checked').val() );
+			branch = $('input[name=browse]:checked').val()
+			switch(branch) {
+				case "song":
+					alert("songs");
+					break;
+				case "date":
+					alert("date");
+					break;
+				case "venue":
+					alert("venue");
+					break;
+				case "recording_type":
+					alert("recording type");
+					break;
+			}
 		});		
 		
 	});
