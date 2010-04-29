891ecec43db07a680260ed229717810583213732
Matt Dolian <mdolian@engineyard.com>
2010-04-29 12:34:47 -0400

    troubleshooting mp3 job

--- app/jobs/generate_mp3.rb
+++ app/jobs/generate_mp3.rb
@@ -3,7 +3,7 @@ require 'resque'
 class GenerateMp3 < DefaultJob
 
   def self.perform(label)
-    system "/data/ambernet/current/bin/flac2mp3 #{label}"
+    exec "/data/ambernet/current/bin/flac2mp3 #{label}"
   end
   
 end
