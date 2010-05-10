b8d5a1a32afb6211629a99e1cd7b43516b6b930b
Matt Dolian <dolian@MDmbPro.local>
2010-05-10 19:10:27 -0400

    added new monit configs in

--- app/jobs/generate_mp3.rb
+++ app/jobs/generate_mp3.rb
@@ -4,7 +4,7 @@ class GenerateMp3 < DefaultJob
 
   def self.perform(directory)
     begin
-      results = %x[shn2mp3 --target #{directory} #{directory}]
+      results = %x[/data/ambernet/current/bin/audio_conversion/shn2mp3 --target #{directory} #{directory}]
     rescue => msg
       puts "MP3 conversion failed with: #{results} - #{msg}"
     end
